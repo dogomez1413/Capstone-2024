96c5410f1625d4316d0ff30acdbd808fccaf2a60
bpayero <89044097+AnnoyingAlchemist@users.noreply.github.com>
2024-03-26 19:25:34 -0400

    flask test

--- python/test.py
+++ python/test.py
@@ -1,15 +1,12 @@
-from flask import Flask, request, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 
 # Defining the home page of our site
-@app.route("/", methods=["GET", "POST"])  # this sets the route to this page
-def gfg():
-    if request.method == "POST":
-        date = request.form.get("date")
-        return "the form date is: " + date
-    return render_template("index.html")
+@app.route("/")  # this sets the route to this page
+def home():
+    return "Hello! this is the main page <h1>HELLO</h1>"  # some basic inline html
 
 
 if __name__ == "__main__":
