b222fe52d88a7979577c7433b0a0e903b29a7b19
dogomez1413 <dogomez@usca.edu>
2024-03-28 19:21:07 -0400

    added lines to include style sheet

--- python/main.py
+++ python/main.py
@@ -6,6 +6,7 @@ def create_flight_table(start, destination, start_date, end_date):
     result = Scrape(start, destination, start_date, end_date)  # obtain our scrape object, represents out query
     ScrapeObjects(result)  # runs selenium through ChromeDriver, modifies results in-place
     result.data.to_html('python/templates/Test_Data.html')  # save the data to a html file
+    
 
 
 # only run for testing if this is main file
@@ -15,6 +16,9 @@ if __name__ == '__main__':
     # pd.set_option('display.width', 1000)
     # Scrape('ATL', 'MYR', '2024-04-22', '2024-04-24')
     create_flight_table('MYR', 'GSP', '2024-04-22', '2024-04-24')
+    func = open('python/templates/Test_Data.html','a')
+    func.write("<link rel='stylesheet' href='styles.css'>")
+    func.close()
 '''
 with pd.option_context('display.max_rows', 3, 'display.max_columns', None):
     display(result.data)
