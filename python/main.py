e04e2dc642d9e81c4d22f09f66a1284bff73540b
dogomez1413 <dogomez@usca.edu>
2024-03-28 17:13:47 -0400

    updated main to output file in correct location

--- python/main.py
+++ python/main.py
@@ -5,7 +5,7 @@ from google_flight_analysis.scrape import *
 def create_flight_table(start, destination, start_date, end_date):
     result = Scrape(start, destination, start_date, end_date)  # obtain our scrape object, represents out query
     ScrapeObjects(result)  # runs selenium through ChromeDriver, modifies results in-place
-    result.data.to_html('Test_Data.html')  # save the data to a html file
+    result.data.to_html('python/templates/Test_Data.html')  # save the data to a html file
 
 
 # only run for testing if this is main file
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     # pd.set_option('display.max_columns', 1000)
     # pd.set_option('display.width', 1000)
     # Scrape('ATL', 'MYR', '2024-04-22', '2024-04-24')
-    create_flight_table('ATL', 'MYR', '2024-04-22', '2024-04-24')
+    create_flight_table('GSP', 'MYR', '2024-04-22', '2024-04-24')
 '''
 with pd.option_context('display.max_rows', 3, 'display.max_columns', None):
     display(result.data)
