94c5dd00085a79230b90740b53c901041a5f5c14
dogomez1413 <dogomez@usca.edu>
2024-03-28 19:28:10 -0400

    removed unneeded code

--- python/main.py
+++ python/main.py
@@ -5,12 +5,7 @@ from google_flight_analysis.scrape import *
 def create_flight_table(start, destination, start_date, end_date):
     result = Scrape(start, destination, start_date, end_date)  # obtain our scrape object, represents out query
     ScrapeObjects(result)  # runs selenium through ChromeDriver, modifies results in-place
-<<<<<<< HEAD
-    result.data.to_html('python/templates/Test_Data.html')  # save the data to a html file
-    
-=======
     result.data.to_html('python/static/Test_Data.html')  # save the data to a html file
->>>>>>> a162e262ded52e2f23d93217690f6ca993643ba6
 
 
 # only run for testing if this is main file
@@ -20,9 +15,6 @@ if __name__ == '__main__':
     # pd.set_option('display.width', 1000)
     # Scrape('ATL', 'MYR', '2024-04-22', '2024-04-24')
     create_flight_table('MYR', 'GSP', '2024-04-22', '2024-04-24')
-    func = open('python/templates/Test_Data.html','a')
-    func.write("<link rel='stylesheet' href='styles.css'>")
-    func.close()
 '''
 with pd.option_context('display.max_rows', 3, 'display.max_columns', None):
     display(result.data)
