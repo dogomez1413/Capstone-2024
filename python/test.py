7dea50a02ba37ae121639a572b0c257b80d9e790
bpayero <89044097+AnnoyingAlchemist@users.noreply.github.com>
2024-03-28 18:30:47 -0400

    Displays table properly

--- python/test.py
+++ python/test.py
@@ -5,12 +5,11 @@ from flask import render_template, request
 app = Flask(__name__) 
 
 
-@app.route("/") 
+@app.route("/", methods=["GET","POST"]) 
 def hello(): 
-	print(request.args)
+	print(request.form.get("date"))
 	message = "Hello, World"
-	return render_template('index.html', 
-						message=message) 
+	return render_template('index.html', message=message) 
 
 # run the application 
 if __name__ == "__main__": 
