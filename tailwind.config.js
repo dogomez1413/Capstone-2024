fa62a237091a31d48bb86a3e9c2502eaeddd8636
gpan1413 <dogomez@usca.edu>
2024-04-11 15:50:14 -0400

    attempting to incorporate tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./python/static/Flight_Data.html", "./python/templates/index.html", "./python/templates/index.html"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
